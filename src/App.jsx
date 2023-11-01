5ef0649e92939b67a0d02f11dc3b5bdf59193550
Abdul <textabdulhere@gmail.com>
2023-11-01 07:01:12 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section className="app">
+        <section className="content"></section>
+      </section>
+    </>
+  );
+}
+
+export default App;
