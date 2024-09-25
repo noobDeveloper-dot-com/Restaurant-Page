975f477d1305644171bdb06775c186c7cab218d4
Abdul <abdul@365.lk>
2024-09-25 12:08:26 +0530

    Implemented Nav bar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <section className="app">
+        <Navbar />
         <section className="content"></section>
       </section>
     </>
